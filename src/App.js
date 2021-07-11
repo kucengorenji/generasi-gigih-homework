5ed8487156457e46b9b3230c781e67d1802b09ab
kucengorenji <mikelrisyad@gmail.com>
2021-07-11 22:26:27 +0700

    finalize the api key and ignore the env local

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
