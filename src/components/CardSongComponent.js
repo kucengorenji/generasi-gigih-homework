b7422486d937120058eafe44a7d5153504be91fa
kucengorenji <mikelrisyad@gmail.com>
2021-07-15 21:32:06 +0700

    succes to create component cardsong

---
+++ src/components/CardSongComponent.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+// import axios from 'axios';
+
+class CardSongComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: []
+    };
+  }
+
+  componentDidMount() {
+
+    fetch("https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, data } = this.state;
+    console.log(data)
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <img src={data.album.images[0].url} alt="" />
+          <p>Track Title : {data.album.name}</p>
+          <p>Artist : {data.album.artists[0].name}</p>
+          <p>Album : {data.name}</p>
+          <button id="submit-button" type="submit">Select</button>
+        </div>
+      );
+    }
+  }
+}
+
+export default CardSongComponent;
