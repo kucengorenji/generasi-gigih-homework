838d2c122299b2daaffc8e623931b0baecbff946
kucengorenji <mikelrisyad@gmail.com>
2021-07-21 14:00:03 +0700

    oauth and search without logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css';
 
 import React from 'react';
-
 import Login from './Pages/Login';
-import PlaylistCard from './Pages/Playlist';
+// import PlaylistCard from './Pages/Playlist';
 
 export default function App() {
 
+  
   return (
     <div className="App" >
       <Login  />
-
       {/* <PlaylistCard /> */}
     </div>
     
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,45 +1,102 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import './Login.css';
 
-
-/*
-    GET https://accounts.spotify.com/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09
-
-
-*/
-
-const CLIENT_ID = "dc0ed7403bb84c5587f67cbe75df1821";
-// const clientSecret = "2c4fca51a8714b4cab1b84673c8374e2";
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_ID;
 const SPOTIFY_AUTHORIZE_ENDPOINT = " https://accounts.spotify.com/authorize";
-const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/app";
-
-const getReturnedParamsFromSpotifyAuth = (hash) => {
-    const stringAfterHashtag = hash.substring(1);
-    const paramsInUrl = stringAfterHashtag.split("&");
-    const paramsSplitItUp = paramsInUrl.reduce((accumulater, currentValue) => {
-        console.log(currentValue);
-        const [key, value] = currentValue.split("=");
-        accumulater[key] = value;
-        return accumulater;
-    }, {});
-
-    return paramsSplitItUp;
-}
-
+const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/";
 const SPACE_DELIMITER = "%20";
-const SCOPES = ["user-read-currently-playing", "user-read-playback-state"];
-const SCOPES_URL_PARAMS =   SCOPES.join(SPACE_DELIMITER);
+const SCOPE = ["playlist-modify-private"];
+const SCOPES_URL_PARAMS =   SCOPE.join(SPACE_DELIMITER);
 
 
 export default function Login() {
+
+    const [token, setToken] = useState("");
+    const [search, setSearch] = useState("");
+    const [datas, setDatas] = useState([]);
 
     const handleLogin = () => {
         window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAMS}&response_type=token&show_dialog=true`
     }
 
+
+    const getTokenFromUrl = (hash) => {
+        const stringAfterHastag = hash.substring(1);
+        const paramInUrl = stringAfterHastag.split("&");
+        const paramSplitUp = paramInUrl.reduce((acc, currentValue) => {
+        const [key, value] = currentValue.split("=");
+        acc[key] = value;
+        return acc;
+        }, {});
+        return paramSplitUp;
+    };
+
+
+    useEffect(() => {
+        if (window.location.hash) {
+        const { access_token } = getTokenFromUrl(window.location.hash);
+        setToken(access_token);
+        }
+    }, []);
+
+    async function HandleSearch() {
+		try {
+			let url =
+				"https://api.spotify.com/v1/search?q=" + search + "&type=track,artist";
+			await axios
+				.get(url, {
+					headers: {
+						Authorization: "Bearer " + token,
+					},
+				})
+				.then((res) => {
+					setDatas(res.data.tracks.items);
+				});
+		} catch (err) {
+			console.error(err);
+		} finally {
+			console.log(datas);
+		}
+	}
+
     return (
         <div>
-            <button onClick={handleLogin} className="login" type="submit">Log In</button>
+            {/* login button */}
+            <div>
+                <button onClick={handleLogin} className="login" type="submit">Log In</button>
+            </div>
+
+            {/* take input */}
+            <div>
+                <input
+                onChange={(e) => {
+                    setSearch(e.target.value);
+                }}
+                value={search}
+                />
+            </div>
+
+            {/* input button */}
+            <button onClick={HandleSearch}>
+                Search
+            </button>
+
+            {/* search result */}
+            <div className="searchResult">
+                {datas.map((data, index) => {
+                    return (
+                    <div className="cardList" key={data.id}>
+                        <img src={data.album.images[2].url} alt={data.name} />
+                        <p>Title: {data.name}</p>
+                        <p>No: {index + 1}</p>
+                        <p>album: {data.album.name}</p>
+                        <p>Release Date: {data.album.release_date}</p>
+                    </div>
+                    );
+                })}
+            </div>
         </div>
+        
     )
 }
